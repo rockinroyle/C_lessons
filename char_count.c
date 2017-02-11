e2c272315e4c67aeedc48f7441da939dd6dd6816
rockinroyle <rockinroyle@gmail.com>
2017-02-11 10:00:35 -0500

    ver 2

--- char_count.c
+++ char_count.c
@@ -3,10 +3,9 @@
 /* count characters in input; ver1 */
 main()
 {
-	long nc;
+	double nc;
 	
-	nc=0;
-	while (getchar() != EOF)
-		++nc;
-	printf("%1d\n", nc);
+	for (nc=0; getchar() !=EOF; ++nc)
+		;
+	printf("%.0f\n", nc);
 }
